c73823df81f691b5bb63cdb8b3b8242ccaae7232
Iryna <batsianiryna@gmail.com>
2022-11-29 10:27:13 +0300

    add task2 задача с координатами и вещественным типом данных

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), причем  X не = 0 и Y не = 0 и выдает номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+
+Console.Write("Введите координаты X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    Console.Write("Точка находится в первой четверти");
+else if (x > 0 && y < 0)
+    Console.Write("Точка находится во второй четверти");
+else if (x < 0 && y < 0)
+    Console.Write("Точка находится в третьей четверти");
+else if (x < 0 && y > 0)
+    Console.Write("Точка находится в четвертой четверти");
+else
+    Console.Write("Данные введены неверно!");   
