fbcebac50feb3358a42bf5871a9e483a2d121e10
Iryna <batsianiryna@gmail.com>
2022-11-30 11:06:59 +0300

    add task4 таблица квадратов от 1 до заданного числа n

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №22. Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+double i = 1;
+while ( i <= n)
+{
+  Console.Write($"{Math.Pow(i,2)}, ");
+  i++;
+}
